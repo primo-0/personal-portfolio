bf3fcf5d09680e5ba93e545b51c97ac4d96a8d55
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-08-04 12:50:13 +0800

    added an eventlistener for scrolling

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/Button";
 import { Menu, X } from "lucide-react";    
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const navLinks = [
     {href:"#about", label:"About"},
     {href:"#projects", label:"Projects"},
@@ -9,8 +9,22 @@ const navLinks = [
 ];
 export const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState (false); 
+    const [isScrolled, setIsScrolled] = useState (false);
+
+    useEffect (()=> {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+            }
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+    <header className={`fixed top-0 left-0 right-0 transition-all duration-500 ${
+        isScrolled ? "glass-strong py-3" : "bg-transparent py-5"}
+        z-50 `}>
         <nav className="container mx-auto px-6 flex items-center justify-between"> 
             <a href="#" 
             className="text-xl font-bold tracking-tight hover:text-primary"
@@ -19,7 +33,7 @@ export const Navbar = () => {
             </a>
             {/* Desktop nav*/}
             <div className ="hidden md:flex items-center gap-1">
-                <div className ="glass rounded-full px2 py-1 flex items-center gap-1">
+                <div className ="glass rounded-full px-2 py-1 flex items-center gap-1">
                     {navLinks.map((link, index) => (
                         <a href={link.href} key={index} className ="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">
                             {link.label}
