f327cc311f504c59b81557eeb0249546c81104ad
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-08-08 13:40:47 +0800

    added download in hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -81,7 +81,10 @@ export const Hero = () => {
                         <Button size="lg">
                             Contact Me <ArrowRight className="w-5 h-5"/>
                         </Button>
-                        <AnimatedBorderButton></AnimatedBorderButton>
+                        <AnimatedBorderButton>
+                             <Download className="w-5 h-5"/>
+                                Download CV
+                        </AnimatedBorderButton>
                     </div>
                     {/* Social Links */}
                     <div className="flex items-center gap-4 animate-fade-in animation-delay-400">
