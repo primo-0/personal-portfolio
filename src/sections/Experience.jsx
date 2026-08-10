5fc48f6c440f379dde74d4265064af6af4daecff
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-08-10 16:48:26 +0800

    added effects for the experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -5,7 +5,7 @@ const experiences = [
         role: " Junior Website Administrator ",
         company: " MGHS Services ",
         description: " Optimized website content through SEO strategies such as keyword research, meta tag optimization, and internal linking. Managed and updated WordPress content while improving website visibility, search performance, and user engagement.",
-        technologies: ["Wordpress",],
+        technologies: ["Wordpress", "ChatGpt", "Javascript"],
         current: false,   
     }, 
 ]
@@ -41,19 +41,29 @@ export const Experience = () => {
                            {/* Experience */}
                     <div className="space-y-12">
                         {experiences.map((exp, idx) => (
-                            <div key={idx}>
+                            <div key={idx} className="relative grid md:grid-cols-2 gap-8 animate-fade-in"
+                                style={{ animationDelay: `${(idx + 1) * 150}ms` }}
+                            >
 
                                 {/* Timeline Dot */}
-                                <div></div>
-                                {/* content */}
-                                <div>
-                                    <div>
-                                        <span>{exp.period}</span>
-                                        <h3>{exp.role}</h3>
-                                        <p>{exp.company}</p>
-                                        <p>{exp.description}</p>
-                                        <div>{exp.technologies.map((tech, techIdx) => (
-                                            <span>{tech}</span>
+                                <div className="absolute left-0 md:left-1/2 top-0 w-3 h-3 bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10">
+                                {exp.current && <span  className="abolute inset-0 rounded-full bg-primary animate-ping opacity-75"/>}
+                                </div>
+                                {/* content */} 
+                                <div className={`pl-8 md:pl-0 ${idx % 2 === 0 ? "md:pd-16 md:text-right" : "md:col-start-2 md:pl-16" }`}>
+                                    <div className={`glass p-6 rounded-2xl border border-primary/30 hover:border-primary/50 transition-all duration-500`}>
+                                        <span className="text-sm text-primary font-medium">{exp.period}</span>
+                                        <h3 className="text-xl font-semibold mt-2">{exp.role}</h3>
+                                        <p className="text-muted-foreground">{exp.company}</p>
+                                        <p className="text-sm text-muted-foreground mt-4">{exp.description}</p>
+                                        <div className={` flex flex-wrap gap-2 mt-4 ${
+                                            idx % 2 === 0 ? "md:justify-end" : ""
+                                        }`}
+                                        >
+                                            {exp.technologies.map((tech, techIdx) => (
+                                            <span 
+                                            key={techIdx}
+                                            className="px-3 py-1 bg-surface text-xs rounded-full text-muted-foreground">{tech}</span>
                                         ))}</div>
                                     </div>
                                 </div>
