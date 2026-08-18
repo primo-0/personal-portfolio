9e1aa547035dd6f16cb238a84394ac7c887225e8
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-08-18 14:42:12 +0800

    fixed mobile interaction on projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -69,11 +69,11 @@ export const Projects = () => {
                             to-transparent opacity-60"
                             />
                             {/* OVERLAYLINKS */}
-                            <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                <a href={project.link} target="_blank" rel="noreferrer" className="p-3 rounded-full glass hover:text-primary-foreground transition-all">
+                            <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 pointer-events-none md:pointer-events-auto">
+                                <a href={project.link} target="_blank" rel="noreferrer" className="p-3 rounded-full glass hover:text-primary-foreground transition-all pointer-events-auto">
                                     <ArrowUpRight className="w-5 h-5"/>
                                 </a>
-                                <a href={project.github} target="_blank" rel="noreferrer" className="p-3 rounded-full glass hover:text-primary-foreground transition-all">
+                                <a href={project.github} target="_blank" rel="noreferrer" className="p-3 rounded-full glass hover:text-primary-foreground transition-all pointer-events-auto">
                                     <FaGithub className="w-5 h-5"/>
                                 </a>
                         </div>
